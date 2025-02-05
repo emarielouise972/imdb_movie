d6d3e4f1d975623e5074dba6989e61c4285b3c40
emarielouise972 <marielouiseesteban90@gmail.com>
2025-02-05 12:11:24 +0100

    visualisation des données

--- imdb_dataset.py
+++ imdb_dataset.py
@@ -1,6 +1,6 @@
 import pandas as pd
-import kagglehub
-
+import matplotlib.pyplot as plt
+import seaborn as sns
 #path = kagglehub.dataset_download("youssefamdouni/imbd-movie-rating")
 path = 'IMBD_movie_rating.csv'
 #load data
@@ -59,3 +59,44 @@ df['Decennie'] = (df['Date'] // 10) * 10
 note_moyenne_decennie = df.groupby('Decennie')['Global_Rating'].mean()
 print('rating par decennie: ', note_moyenne_decennie)
 
+sns.set(style="darkgrid")
+
+plt.figure(figsize=(10, 5))
+sns.countplot(y=df['Gender'], palette="viridis", order=df['Gender'].value_counts().index)
+plt.xlabel("Nombre de films")
+plt.ylabel("Genre")
+plt.title("Répartition des genres de films")
+plt.show()
+
+
+plt.figure(figsize=(8, 5))
+sns.histplot(df['Global_Rating'], bins=20, kde=True, color='blue')
+plt.xlabel("Note Globale")
+plt.ylabel("Nombre de Films")
+plt.title("Distribution des Notes Globales")
+plt.show()
+
+
+plt.figure(figsize=(10, 5))
+sns.countplot(x=df['Decennie'].dropna(), palette="coolwarm", order=sorted(df['Decennie'].dropna().unique()))
+plt.xlabel("Décennie de sortie")
+plt.ylabel("Nombre de films")
+plt.title("Nombre de films par décennie")
+plt.xticks(rotation=45)
+plt.show()
+
+
+plt.figure(figsize=(8, 5))
+sns.scatterplot(x=df['Runtime'], y=df['Global_Rating'], alpha=0.5, color='purple')
+plt.xlabel("Durée du film (minutes)")
+plt.ylabel("Note Globale")
+plt.title("Relation entre la durée et la note globale")
+plt.show()
+
+
+plt.figure(figsize=(10, 5))
+sns.countplot(y=df['Review'], palette="viridis", order=df['Gender'].value_counts().index)
+plt.xlabel("Nombre de films")
+plt.ylabel("Genre")
+plt.title("Répartition des genres de films")
+plt.show()
